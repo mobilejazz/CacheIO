34e1ad5de4e4d35e60bbf20bf828c13b51df9db3
Jose Luis  Franconetti Olmedo <joseluis.franconetti@gmail.com>
2015-11-23 21:46:29 +0100

    Implement the ApplicationTestCase class to configure Roboelectric

---
+++ library/src/test/java/com/mobilejazz/cacheio/ApplicationTestCase.java
@@ -0,0 +1,10 @@
+package com.mobilejazz.cacheio;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21, manifest = Config.NONE)
+public class ApplicationTestCase {
+}
