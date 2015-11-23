c3933d254b08e78e7e3dbe89b57132caa2a31b20
Jose Luis  Franconetti Olmedo <joseluis.franconetti@gmail.com>
2015-11-24 00:48:46 +0100

    Implemented cache value strategies

--- library/src/main/java/com/mobilejazz/cacheio/detector/cacheentry/CacheValueStrategy.java
+++ library/src/main/java/com/mobilejazz/cacheio/detector/cacheentry/CacheValueStrategy.java
@@ -1,6 +1,7 @@
 package com.mobilejazz.cacheio.detector.cacheentry;
 
 import android.support.annotation.Nullable;
+import com.mobilejazz.cacheio.Serializer;
 import com.mobilejazz.cacheio.manager.entity.CacheEntry;
 import com.mobilejazz.cacheio.manager.entity.StoreObject;
 import java.util.List;
@@ -10,10 +11,10 @@ public interface CacheValueStrategy {
   class NullCacheValueStrategy implements CacheValueStrategy {
 
     // TODO: 23/11/15 Do not return a null, to avoid null checks in the contract
-    @Override @Nullable public <T> CacheEntry<T> convert(List<StoreObject> storeObjects) {
+    @Override @Nullable public <T> CacheEntry<T> convert(Serializer serializer, List<StoreObject> storeObjects) {
       return null;
     }
   }
 
-  <T> CacheEntry<T> convert(List<StoreObject> storeObjects);
+  <T> CacheEntry<T> convert(Serializer serializer, List<StoreObject> storeObjects);
 }
--- library/src/main/java/com/mobilejazz/cacheio/detector/cacheentry/ListCacheValueStrategy.java
+++ library/src/main/java/com/mobilejazz/cacheio/detector/cacheentry/ListCacheValueStrategy.java
@@ -1,11 +1,56 @@
 package com.mobilejazz.cacheio.detector.cacheentry;
 
+import com.mobilejazz.cacheio.Serializer;
 import com.mobilejazz.cacheio.manager.entity.CacheEntry;
 import com.mobilejazz.cacheio.manager.entity.StoreObject;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListCacheValueStrategy implements CacheValueStrategy {
-  @Override public <T> CacheEntry<T> convert(List<StoreObject> storeObjects) {
-    return null;
+
+  @SuppressWarnings("unchecked") @Override
+  public <T> CacheEntry<T> convert(Serializer serializer, List<StoreObject> storeObjects) {
+
+    List values = new ArrayList(storeObjects.size());
+
+    String key = null;
+    Class type = null;
+    long expiryMillis = -1;
+
+    for (StoreObject storeObject : storeObjects) {
+
+      try {
+        String typeName = storeObject.getType();
+        byte[] value = storeObject.getValue();
+
+        if (key == null) {
+          key = storeObject.getKey();
+        }
+
+        if (type == null) {
+          type = Class.forName(typeName);
+        }
+
+        if (expiryMillis == -1) {
+          expiryMillis = storeObject.getExpiredMillis();
+        }
+
+        T object = (T) serializer.fromBytes(value, type);
+
+        values.add(object);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+        // TODO: 24/11/15 We should not return a null pointer we should make some cast to a CacheEntry or throw a exception
+        return null;
+      }
+    }
+
+    CacheEntry<T> cacheEntry = new CacheEntry<>();
+    cacheEntry.setKey(key);
+    cacheEntry.setType(type);
+    cacheEntry.setValue((T) values);
+    cacheEntry.setExpiryMillis(expiryMillis);
+
+    return cacheEntry;
   }
 }
