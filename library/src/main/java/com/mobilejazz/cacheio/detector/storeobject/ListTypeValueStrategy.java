4248898b13ef94fff6a1ab4ad5d1f5162bfdfdab
Aldo Borrero González <aldo@aldoborrero.com>
2015-11-23 23:57:22 +0100

    Created TypeValueStrategy to convert a cache entry object to a list of store objects

---
+++ library/src/main/java/com/mobilejazz/cacheio/detector/storeobject/ListTypeValueStrategy.java
@@ -0,0 +1,35 @@
+package com.mobilejazz.cacheio.detector.storeobject;
+
+import com.mobilejazz.cacheio.Serializer;
+import com.mobilejazz.cacheio.manager.entity.CacheEntry;
+import com.mobilejazz.cacheio.manager.entity.StoreObject;
+import com.mobilejazz.cacheio.manager.entity.StoreObjectBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTypeValueStrategy implements TypeValueStrategy {
+  @Override public List<StoreObject> convert(Serializer serializer, CacheEntry cacheEntry) {
+    List list = (List) cacheEntry.getValue();
+    List<StoreObject> storeObjects = new ArrayList<>(list.size());
+    byte[] bytes;
+    String index;
+
+    for (int position = 0; position < list.size(); position++) {
+      bytes = serializer.toBytes(cacheEntry.getValue());
+      index = StoreObject.generateIndex(cacheEntry.getKey(), String.valueOf(position));
+
+      StoreObject storeObject = new StoreObjectBuilder().setKey(cacheEntry.getKey())
+          .setType(cacheEntry.getType().getCanonicalName())
+          .setValue(bytes)
+          .setIndex(index)
+          .setExpiryMillis(cacheEntry.getExpiryMillis())
+          .setMetaType(List.class.getSimpleName())
+          .setTimestamp(System.currentTimeMillis())
+          .build();
+
+      storeObjects.add(storeObject);
+    }
+
+    return storeObjects;
+  }
+}
---
+++ library/src/main/java/com/mobilejazz/cacheio/detector/storeobject/TypeValueStrategy.java
@@ -0,0 +1,21 @@
+package com.mobilejazz.cacheio.detector.storeobject;
+
+import android.support.annotation.NonNull;
+import com.mobilejazz.cacheio.Serializer;
+import com.mobilejazz.cacheio.manager.entity.CacheEntry;
+import com.mobilejazz.cacheio.manager.entity.StoreObject;
+import java.util.Collections;
+import java.util.List;
+
+public interface TypeValueStrategy {
+
+  class NullTypeValueStrategy implements TypeValueStrategy {
+
+    @Override @NonNull public List<StoreObject> convert(Serializer serializer, CacheEntry cacheEntry) {
+      return Collections.emptyList();
+    }
+  }
+
+  List<StoreObject> convert(Serializer serializer, CacheEntry cacheEntry);
+
+}
