4ce38beb6b14443d88117d46bdd5b4d9387891f2
Jose Luis  Franconetti Olmedo <joseluis.franconetti@gmail.com>
2015-11-23 18:59:17 +0100

    Added Persistence interface to abstract all the persistence strategy.

---
+++ library/src/main/java/com/mobilejazz/cacheio/Persitence.java
@@ -0,0 +1,21 @@
+package com.mobilejazz.cacheio;
+
+import com.mobilejazz.cacheio.exceptions.CacheErrorException;
+import com.mobilejazz.cacheio.exceptions.CacheNotFoundException;
+import com.mobilejazz.cacheio.exceptions.ExpiredCacheException;
+import com.mobilejazz.cacheio.manager.entity.StoreObject;
+import com.mobilejazz.cacheio.strategy.CachingStrategy;
+import com.mobilejazz.cacheio.strategy.CachingStrategyObject;
+import java.util.List;
+
+public interface Persitence {
+
+  List<StoreObject> obtain(String key) throws CacheNotFoundException, CacheErrorException;
+
+  boolean persist(List<StoreObject> value) throws CacheErrorException;
+
+  boolean delete(String key) throws CacheErrorException;
+
+  <T extends CachingStrategyObject> void executeValidation(StoreObject storeObject,
+      CachingStrategy<T> strategy) throws ExpiredCacheException, CacheNotFoundException;
+}
