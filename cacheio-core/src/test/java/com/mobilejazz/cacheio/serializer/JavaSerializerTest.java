e6b7b5ce8b401efdae201e8a070c634dd23150d7
Jose Luis  Franconetti Olmedo <joseluis.franconetti@gmail.com>
2015-12-08 20:20:53 +0100

    Created java serializer test class

---
+++ cacheio-core/src/test/java/com/mobilejazz/cacheio/serializer/JavaSerializerTest.java
@@ -0,0 +1,12 @@
+package com.mobilejazz.cacheio.serializer;
+
+import com.mobilejazz.cacheio.ApplicationTestCase;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class JavaSerializerTest extends ApplicationTestCase {
+
+  @Test public void shouldPassTheTest() throws Exception {
+    Assertions.assertThat(true).isTrue();
+  }
+}
