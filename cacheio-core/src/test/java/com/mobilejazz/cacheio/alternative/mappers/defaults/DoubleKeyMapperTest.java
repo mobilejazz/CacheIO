61eb0e14f0fb04d156a978c4925bf76ec6d6901b
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2016-03-03 22:39:48 +0100

    Implemented tests to the DoubleKeyMapper

---
+++ cacheio-core/src/test/java/com/mobilejazz/cacheio/alternative/mappers/defaults/DoubleKeyMapperTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 Mobile Jazz
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.mobilejazz.cacheio.alternative.mappers.defaults;
+
+import com.mobilejazz.cacheio.alternative.mappers.KeyMapper;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DoubleKeyMapperTest {
+
+  private final KeyMapper<Double> mapper = new DoubleKeyMapper();
+
+  @Test(expected = IllegalArgumentException.class) @SuppressWarnings("ResultOfMethodCallIgnored")
+  public void shouldThrowIllegalArgumentExceptionIfMappingToStringTheValueIsNull() {
+    mapper.toString(null);
+  }
+
+  @Test(expected = IllegalArgumentException.class) @SuppressWarnings("ResultOfMethodCallIgnored")
+  public void shouldThrowIllegalArgumentExceptionIfMappingFromStringTheValueIsNull() {
+    mapper.fromString(null);
+  }
+
+  @Test public void shouldMappingToStringAValue() throws Exception {
+    String result = mapper.toString(1.2);
+
+    assertThat(result).isEqualTo("1.2");
+  }
+
+  @Test public void shouldMappingFromStringAValue() throws Exception {
+    Double result = mapper.fromString("1.2");
+
+    assertThat(result).isEqualTo(1.2);
+  }
+}
