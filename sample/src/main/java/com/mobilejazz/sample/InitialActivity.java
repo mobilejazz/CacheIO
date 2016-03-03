ee26628c58a2327525869afb31126cce68d444a6
Brian McGee <b.m.mcgee@gmail.com>
2016-03-03 21:56:59 +0100

    Fixed some more build issues

--- sample/src/main/java/com/mobilejazz/sample/InitialActivity.java
+++ sample/src/main/java/com/mobilejazz/sample/InitialActivity.java
@@ -3,176 +3,78 @@ package com.mobilejazz.sample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-
-import com.mobilejazz.cacheio.alternative.RxCache;
-import com.mobilejazz.cacheio.serializers.gson.GsonValueMapper;
+import com.mobilejazz.cacheio.Cache;
+import com.mobilejazz.cacheio.CacheIO;
+import com.mobilejazz.cacheio.logging.LogLevel;
+import com.mobilejazz.cacheio.manager.entity.CacheEntry;
+import com.mobilejazz.cacheio.serializers.gson.GsonSerializer;
 import com.mobilejazz.sample.gson.GsonFactory;
 import com.mobilejazz.sample.model.User;
-
-import java.util.concurrent.TimeUnit;
-
-import rx.Single;
-import rx.SingleSubscriber;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InitialActivity extends AppCompatActivity {
 
-    private static final String TAG = InitialActivity.class.getSimpleName();
+  private static final String TAG = InitialActivity.class.getSimpleName();
+  public static final String USER_KEY_LIST = "user.key.list";
+  public static final String USER_KEY_ONE = "user.key.one";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_initial);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_initial);
 
-        final RxCache<Integer, User> rxCache = SQLiteRxCache.newBuilder(Integer.class, User.class)
-                .setContext(getApplicationContext())
-                .setDatabaseName("CacheDatabase2")
-                .setTableName("Users")
-                .setValueMapper(new GsonValueMapper(GsonFactory.create()))
-                .setVersionMapper(new User.VersionMapper())
-                .setScheduler(Schedulers.io())
-                .build();
+    CacheIO cacheIO = CacheIO.with(getApplicationContext())
+        .identifier("cacheio-test")
+        .logLevel(LogLevel.FULL)
+        .serializer(new GsonSerializer(GsonFactory.create()))
+        .build();
 
-        final User userOne = new User()
-                .setId(1)
-                .setName("Jose Luis Franconetti")
-                .setVersion(1L);
+    Cache cache = cacheIO.cacheDataSource();
 
-//        final User userTwo = new User()
-//                .setId(2)
-//                .setName("Test user")
-//                .setVersion(1L);
-//
-//        final Map<Integer, User> entries = new HashMap<>();
-//        entries.put(userOne.getId(), userOne);
-//        entries.put(userTwo.getId(), userTwo);
-//
-//        try {
-//
-//            // Note the put isn't carried out until you call subscribe on the Single<T> object which is returned
-//            // This is due to the nature of the Observable contract
-//            final Single<Map<Integer, User>> putOp = rxCache.putAll(entries, 1, TimeUnit.SECONDS);
-//
-//            final CountDownLatch putLatch = new CountDownLatch(1);
-//
-//            putOp.subscribe(new SingleSubscriber<Map<Integer, User>>() {
-//                @Override
-//                public void onSuccess(Map<Integer, User> entries) {
-//                    Log.d(TAG, "Successfully inserted user entries");
-//                    putLatch.countDown();
-//                }
-//
-//                @Override
-//                public void onError(Throwable error) {
-//                    Log.e(TAG, "Failed to insert user entries", error);
-//                }
-//            });
-//
-//
-//            putLatch.await();
-//
-//            final CountDownLatch getLatch = new CountDownLatch(1);
-//
-//            final List<Integer> keys = new ArrayList<>();
-//            keys.add(1);
-//            keys.add(2);
-//
-//            final Single<Map<Integer, User>> getOp = rxCache.getAll(keys);
-//
-//            getOp.subscribe(new SingleSubscriber<Map<Integer, User>>() {
-//                @Override
-//                public void onSuccess(Map<Integer, User> values) {
-//                    Log.d(TAG, "Retrieved entries");
-//                    Log.d(TAG, "User 1 matches = " + userOne.equals(values.get(1)));
-//                    Log.d(TAG, "User 2 matches = " + userTwo.equals(values.get(2)));
-//                    getLatch.countDown();
-//                }
-//
-//                @Override
-//                public void onError(Throwable error) {
-//                    Log.e(TAG, "Failed to get user entries", error);
-//                }
-//            });
-//
-//            getLatch.await();
-//
-//            // wait 5 seconds and allow the entries to expire
-//            Thread.sleep(5000);
-//
-//            final Single<Map<Integer, User>> expiredGetOp = rxCache.getAll(keys);
-//
-//            expiredGetOp.subscribe(new SingleSubscriber<Map<Integer, User>>() {
-//                @Override
-//                public void onSuccess(Map<Integer, User> values) {
-//                    Log.d(TAG, "Entries have expired = " + values.isEmpty());
-//                }
-//
-//                @Override
-//                public void onError(Throwable error) {
-//                    Log.e(TAG, "Failed to get user entries", error);
-//                }
-//            });
-//
-//        } catch(Throwable t){
-//            t.printStackTrace();
-//        }
-//
+    User userOne = new User();
+    userOne.setId(1);
+    userOne.setName("Jose Luis Franconetti");
 
-        userOne.setName("Version 3")
-                .setVersion(3L);
+    User userTwo = new User();
+    userTwo.setId(2);
+    userTwo.setName("Test user");
 
-        rxCache.remove(userOne.getId())
-                .flatMap(new Func1<Integer, Single<User>>() {
-                    @Override
-                    public Single<User> call(Integer integer) {
-                        Log.d(TAG, "User removed = " + integer);
-                        return rxCache.put(userOne.getId(), userOne, Long.MAX_VALUE, TimeUnit.SECONDS);
-                    }
-                })
-                .flatMap(new Func1<User, Single<User>>() {
-                    @Override
-                    public Single<User> call(User user) {
+    CacheEntry cacheEntryUserOne = CacheEntry.create(USER_KEY_ONE, User.class, userOne, 12);
+    cache.persist(cacheEntryUserOne);
+    cache.persist(cacheEntryUserOne);
+    cache.persist(cacheEntryUserOne);
 
-                        Log.d(TAG, "User inserted = " + user);
+    List<User> users = new ArrayList<>();
+    users.add(userOne);
+    users.add(userTwo);
 
-                        // try to insert an old version
+    CacheEntry cacheEntryUserList = CacheEntry.create(USER_KEY_LIST, User.class, users, 12);
+    cache.persist(cacheEntryUserList);
+    cache.persist(cacheEntryUserList);
+    cache.persist(cacheEntryUserList);
 
-                        return rxCache.put(userOne.getId(), userOne.setName("version 2").setVersion(2L), Long.MAX_VALUE, TimeUnit.SECONDS);
-                    }
-                })
-                .flatMap(new Func1<User, Single<User>>() {
-                    @Override
-                    public Single<User> call(User user) {
+    CacheEntry resultQueryUserOne = cache.obtain(USER_KEY_ONE);
 
-                        // Should return null because it didn't insert. Could make this throw an exception instead
-                        Log.d(TAG, "Failed to insert old user = " + (user == null));
+    User resultUser = (User) resultQueryUserOne.getValue();
+    Log.d(TAG, "User One = " + resultUser.toString());
 
-                        // try to insert a newer version
-                        return rxCache.put(userOne.getId(), userOne.setName("version 4").setVersion(4L), Long.MAX_VALUE, TimeUnit.SECONDS);
-                    }
-                })
-                .map(new Func1<User, User>() {
-                    @Override
-                    public User call(User user) {
-                        Log.d(TAG, "User inserted = " + user);
-                        return user;
-                    }
-                })
-                // need to subscribe to force the execution
-                .subscribe(new SingleSubscriber<User>() {
-                    @Override
-                    public void onSuccess(User value) {
-                        Log.d(TAG, "Finished");
-                    }
-
-                    @Override
-                    public void onError(Throwable error) {
-                        Log.e(TAG, "Error", error);
-                    }
-                });
+    CacheEntry resultQueryUserList = cache.obtain(USER_KEY_LIST);
 
 
+    List<User> resultQueryListUsers = (List<User>) resultQueryUserList.getValue();
+    for (User user : resultQueryListUsers) {
+      Log.d(TAG, "List - " + user.toString());
     }
+
+/*    User userThree = new User();
+    userThree.setId(3);
+    userThree.setName("Aldo Borrero");*/
+
+    //CacheEntry cacheEntryUserThree = CacheEntry.create("user.key.three", User.class, userThree);
+    //cache.persist(cacheEntryUserThree);
+    //cache.delete("user.key.three");
+
+    //CacheEntry emptyUserThreeEntry = cache.obtain("user.key.three");
+    //Log.d(TAG, "Cache is emptied for User three: " + String.valueOf(emptyUserThreeEntry == null));
+  }
 }
--- sample/src/main/java/com/mobilejazz/sample/model/User.java
+++ sample/src/main/java/com/mobilejazz/sample/model/User.java
@@ -1,83 +1,38 @@
 package com.mobilejazz.sample.model;
 
-import com.mobilejazz.cacheio.alternative.RxCache;
-
 public class User {
 
-    private int id;
-    private String name;
-    private long version = 1L;
+  private int id;
+  private String name;
 
-    public User(int id, String name, long version) {
-        this.id = id;
-        this.name = name;
-        this.version = version;
-    }
+  public User(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 
-    public User() {
-    }
+  public User() {
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public User setId(int id) {
-        this.id = id;
-        return this;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public User setName(String name) {
-        this.name = name;
-        return this;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public long getVersion() {
-        return version;
-    }
-
-    public User setVersion(long version) {
-        this.version = version;
-        return this;
-    }
-
-    public static final class VersionMapper implements RxCache.VersionMapper<User> {
-        @Override
-        public long getVersion(User model) {
-            return model.getVersion();
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", version=" + version +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (id != user.id) return false;
-        if (version != user.version) return false;
-        return !(name != null ? !name.equals(user.name) : user.name != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (version ^ (version >>> 32));
-        return result;
-    }
+  @Override public String toString() {
+    return "User{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        '}';
+  }
 }
