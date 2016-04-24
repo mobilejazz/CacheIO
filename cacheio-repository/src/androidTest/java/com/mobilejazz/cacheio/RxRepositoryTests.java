af1da64d98fa5d73145b6f9f497adbd72a284113
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2016-04-24 20:00:34 +0200

    Implemented tests for the DefaultQuery with the Repository

--- cacheio-repository/src/androidTest/java/com/mobilejazz/cacheio/RxRepositoryTests.java
+++ cacheio-repository/src/androidTest/java/com/mobilejazz/cacheio/RxRepositoryTests.java
@@ -19,8 +19,10 @@ package com.mobilejazz.cacheio;
 import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import com.mobilejazz.cacheio.mappers.DefaultQueryMapper;
 import com.mobilejazz.cacheio.mappers.PaginatedQueryMapper;
 import com.mobilejazz.cacheio.model.TestUser;
+import com.mobilejazz.cacheio.query.DefaultQuery;
 import com.mobilejazz.cacheio.query.PaginatedQuery;
 import org.junit.Before;
 import org.junit.Test;
@@ -43,33 +45,38 @@ import static org.assertj.core.api.Assertions.assertThat;
         .identifier("test.database")
         .executor(Executors.newSingleThreadExecutor())
         .setKeyMapper(PaginatedQuery.class, new PaginatedQueryMapper())
+        .setKeyMapper(DefaultQuery.class, new DefaultQueryMapper())
         .build();
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenDummyObjectIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.put(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenQueryAndListAreNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.put(null, null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenPutAndQueryIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.put(null, Collections.<TestUser>emptyList());
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenPutAndListIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     PaginatedQuery paginatedQuery = givenAFakePaginatedQuery("test", 0, 5);
 
@@ -78,48 +85,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenFindAndQueryIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.find(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenFindByIdAndIdIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.findById(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenFindByIdAndIdIsEmpty() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.findById("");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenRemoveByIdAndIdIsEmpty() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.removeById("");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenRemoveByIdAndIdIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.removeById(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void shouldThrowAExceptionWhenRemoveByQueryAndQueryIsNull() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     repository.removeByQuery(null);
   }
 
   @Test public void shouldPutADummyObject() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     TestUser testUserPut = repository.put(givenDummyTestUser()).toObservable().toBlocking().first();
 
@@ -133,7 +147,8 @@ import static org.assertj.core.api.Assertions.assertThat;
   }
 
   @Test public void shouldPutAListOfDummyObjectsWithPaginatedQuery() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     TestUser testUserOne = givenDummyTestUser("one", "dummy_one");
     TestUser testUserTwo = givenDummyTestUser("two", "dummy_two");
@@ -161,9 +176,38 @@ import static org.assertj.core.api.Assertions.assertThat;
     assertThat(testUsersExpected.get(1).getName()).isEqualTo(testUserTwo.getName());
   }
 
+  @Test public void shouldPutAListOfDummyObjectsWithDefaultQuery() throws Exception {
+    RxRepository<String, TestUser, DefaultQuery> repository = givenARxWithDefaultQueryRepository();
+
+    TestUser testUserOne = givenDummyTestUser("one", "dummy_one");
+    TestUser testUserTwo = givenDummyTestUser("two", "dummy_two");
+
+    List<TestUser> testUsersToPut = new ArrayList<>(2);
+    testUsersToPut.add(testUserOne);
+    testUsersToPut.add(testUserTwo);
+
+    DefaultQuery query = new DefaultQuery("test.query");
+    List<TestUser> first =
+        repository.put(query, testUsersToPut).toObservable().toBlocking().first();
+
+    List<TestUser> testUsersExpected = repository.find(query).toObservable().toBlocking().first();
+
+    assertThat(testUsersExpected).isNotNull();
+    assertThat(testUsersExpected.size()).isEqualTo(testUsersToPut.size());
+
+    assertThat(testUsersExpected.get(0)).isNotNull();
+    assertThat(testUsersExpected.get(0).getId()).isEqualTo(testUserOne.getId());
+    assertThat(testUsersExpected.get(0).getName()).isEqualTo(testUserOne.getName());
+
+    assertThat(testUsersExpected.get(1)).isNotNull();
+    assertThat(testUsersExpected.get(1).getId()).isEqualTo(testUserTwo.getId());
+    assertThat(testUsersExpected.get(1).getName()).isEqualTo(testUserTwo.getName());
+  }
+
   @Test public void shouldReturnEmptyListWhenThereIsNoDummyObjectsWithPaginatedQuery()
       throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     List<TestUser> testUsers = repository.find(givenAFakePaginatedQuery("fake.id", 0, 5))
         .toObservable()
@@ -173,8 +217,19 @@ import static org.assertj.core.api.Assertions.assertThat;
     assertThat(testUsers).isEmpty();
   }
 
+  @Test public void shouldReturnEmptyListWhenThereIsNoDummyObjectsWithDefaultQuery()
+      throws Exception {
+    RxRepository<String, TestUser, DefaultQuery> repository = givenARxWithDefaultQueryRepository();
+
+    List<TestUser> testUsers =
+        repository.find(givenADefaultQuery("test.id")).toObservable().toBlocking().first();
+
+    assertThat(testUsers).isEmpty();
+  }
+
   @Test public void shouldRemoveADummyObject() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     TestUser persisted = repository.put(givenDummyTestUser()).toObservable().toBlocking().first();
 
@@ -186,7 +241,8 @@ import static org.assertj.core.api.Assertions.assertThat;
   }
 
   @Test public void shouldRemoveAListOfDummyObjectsByAPaginatedQuery() throws Exception {
-    RxRepository<String, TestUser, PaginatedQuery> repository = givenARxRepository();
+    RxRepository<String, TestUser, PaginatedQuery> repository =
+        givenARxWithPaginatedQueryRepository();
 
     TestUser testUserOne = givenDummyTestUser("one", "dummy_one");
     TestUser testUserTwo = givenDummyTestUser("two", "dummy_two");
@@ -201,6 +257,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
     Collection<String> keysRemoved =
         repository.removeByQuery(paginatedQuery).toObservable().toBlocking().first();
+
+    assertThat(keysRemoved).isNotNull();
+  }
+
+  @Test public void shouldRemoveAListOfDummyObjectsByADefaultQuery() throws Exception {
+    RxRepository<String, TestUser, DefaultQuery> repository = givenARxWithDefaultQueryRepository();
+
+    TestUser testUserOne = givenDummyTestUser("one", "dummy_one");
+    TestUser testUserTwo = givenDummyTestUser("two", "dummy_two");
+
+    List<TestUser> testUsersToPut = new ArrayList<>(2);
+    testUsersToPut.add(testUserOne);
+    testUsersToPut.add(testUserTwo);
+
+    DefaultQuery query = givenADefaultQuery("test.fake.id");
+    List<TestUser> testUsersPut =
+        repository.put(query, testUsersToPut).toObservable().toBlocking().first();
+
+    Collection<String> keysRemoved =
+        repository.removeByQuery(query).toObservable().toBlocking().first();
 
     assertThat(keysRemoved).isNotNull();
   }
@@ -225,7 +301,7 @@ import static org.assertj.core.api.Assertions.assertThat;
     return givenDummyTestUser(FAKE_TEST_USER_ID, FAKE_TEST_USER_NAME);
   }
 
-  private RxRepository<String, TestUser, PaginatedQuery> givenARxRepository() {
+  private RxRepository<String, TestUser, PaginatedQuery> givenARxWithPaginatedQueryRepository() {
     RxCache<String, TestUser> cache = cacheIO.newRxCache(String.class, TestUser.class);
 
     RxCache<PaginatedQuery, StringList> queryCache =
@@ -236,8 +312,23 @@ import static org.assertj.core.api.Assertions.assertThat;
         .build();
   }
 
+  private RxRepository<String, TestUser, DefaultQuery> givenARxWithDefaultQueryRepository() {
+    RxCache<String, TestUser> cache = cacheIO.newRxCache(String.class, TestUser.class);
+
+    RxCache<DefaultQuery, StringList> queryCache =
+        cacheIO.newRxCache(DefaultQuery.class, StringList.class);
+
+    return new StringKeyedRxRepository.Builder<TestUser, DefaultQuery>().setCache(cache)
+        .setQueryCache(queryCache)
+        .build();
+  }
+
   @NonNull private PaginatedQuery givenAFakePaginatedQuery(String test, int offset, int limit) {
     return new PaginatedQuery(test, offset, limit);
+  }
+
+  @NonNull private DefaultQuery givenADefaultQuery(String id) {
+    return new DefaultQuery(id);
   }
 
 }
